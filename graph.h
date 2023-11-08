dbec5dc12a95f678b908ab496f53961817c923bb
Evan <103800106+eggrollqq@users.noreply.github.com>
2023-11-08 11:07:45 -0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -5,7 +5,8 @@
 
 void insertIntoAdjacencyList(pNODE* ADJ, int u, int v, double w, int edgeIndex, int flag);
 void PrintADJ(pNODE* ADJ, int n);
-void dijkstra(pVERTEX *V, pNODE *ADJ, int n, int source);
 void printPath(pVERTEX *V, int source, int destination);
+HEAP *InitializeHeap(int capacity);
+void dijkstra(pVERTEX *V, pNODE *ADJ, int n, int source, int target);
 
 #endif
