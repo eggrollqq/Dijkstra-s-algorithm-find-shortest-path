5aefbf0bcbdf8b5093b78cea4e36fdb7ef24ea3a
Evan <103800106+eggrollqq@users.noreply.github.com>
2023-11-09 09:14:03 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -1,18 +1,19 @@
 
+
 #ifndef _heap_H
 #define _heap_H 1
 #include "data_structures.h"
 
-extern int heapifyCalls;
 
-void swap(ELEMENT **a, ELEMENT **b);
-void Heapify(HEAP *h, int i);
-void BuildHeap(HEAP *h, ELEMENT **A, int n);
-ELEMENT *ExtractMin(HEAP *h);
-void DecreaseKey(HEAP *h, int i, double newKey);
-void Insertion(HEAP *h, ELEMENT *x);
+
+void swap(VERTEX **a, VERTEX **b);
 bool IsMinHeap(HEAP *heap);
-void freeHeap(HEAP* h);
+void Heapify(HEAP *h, int i);
+void BuildHeap(HEAP *h, VERTEX **A, int n);
+VERTEX *ExtractMin(HEAP *h);
+void DecreaseKey(HEAP *h, int i, double newKey);
+void Insertion(HEAP *h, VERTEX *x);
+void freeHeap(HEAP *h);
 
 
 
