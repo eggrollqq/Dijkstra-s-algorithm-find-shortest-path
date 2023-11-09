9d50d6103260f57213020a7f752a9ecb586a1bcb
Evan <103800106+eggrollqq@users.noreply.github.com>
2023-11-09 09:14:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,91 +7,89 @@
 #include "main.h"
 #include "stack.h"
 #include "graph.h"
+#include <cfloat>
 
-int lastSrc = -1;
-int lastDest = -1;
 
-int main(int argc, char **argv){
-    FILE *fp;
-    ELEMENT *element = NULL;
-    HEAP *heap = NULL;
-    LISTSTACK *liststack = NULL;
-    VERTEX *vertex = NULL;
-    NODE *node = NULL;
-    COLOR *color = NULL;
-    int flag;
-
-    int key;
-    int returnV;
-    char Word[100];
-
-     if (argc > 3) {
+int main(int argc, char **argv) {
+    
+    int flag = 0; // For directed/undirected graph
+    if (argc > 3) {
         flag = atoi(argv[3]);
         if (flag < 1 || flag > 2) { 
             fprintf(stderr, "Error: invalid flag value\n");
             return 1;  
         }
      }
-     
+    
+    
+    flag = atoi(argv[3]);
 
-    fp = NULL;
-
-    fp = fopen(argv[1], "r");
-
-    if (!fp){
+    // Open file
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp) {
         fprintf(stderr, "Error: cannot open file %s\n", argv[1]);
-        exit(0);
+        return 1;
     }
 
-
-
-    int n, m;  // number of vertices and edges
-    pVERTEX* V = NULL;  // array of pointers to VERTEX
-    pNODE* ADJ = NULL;  // array of adjacency lists
-
+    int n, m; // number of vertices and edges
     fscanf(fp, "%d %d", &n, &m);
 
     // Allocate memory for V and ADJ
-V = (pVERTEX*) malloc((n + 1) * sizeof(pVERTEX));
-ADJ = (pNODE*) malloc((n + 1) * sizeof(pNODE));
+    VERTEX **V = (VERTEX **)malloc((n + 1) * sizeof(VERTEX *));
+    NODE **ADJ = (NODE **)malloc((n + 1) * sizeof(NODE *));
 
-if (V == NULL) {
-    fprintf(stderr, "Error: Memory allocation failed for V\n");
-    // Handle the error or exit
-}
-
-    // Initialize all pointers in V and ADJ to NULL
-    for (int i = 1; i <= n; i++) {
-        V[i] = (pVERTEX) malloc(sizeof(VERTEX));
-        V[i]->position = i;
-        ADJ[i] = NULL;
+    if (!V || !ADJ) {
+        fprintf(stderr, "Error: Memory allocation failed\n");
+        fclose(fp);
+        free(V);
+        free(ADJ);
+        return 1;
     }
 
-    
+    // Initialize vertices and adjacency lists
+    // Inside the main function after allocating V and ADJ
+    for (int i = 1; i <= n; i++) {
+        V[i] = (VERTEX *)malloc(sizeof(VERTEX));
+        if (!V[i]) {
+        // Free previously allocated memory
+            for (int j = 1; j < i; j++) {
+            free(V[j]);
+        }
+            free(V);
+            free(ADJ);
+            fclose(fp);
+            fprintf(stderr, "Error: Memory allocation failed for vertex\n");
+            return 1;
+    }
 
-    // Read edges and populate the adjacency lists
+    V[i]->position = i;
+    V[i]->d = DBL_MAX;
+    V[i]->pi = -1;
+    ADJ[i] = NULL;
+}
+
+    // Read edges and populate adjacency lists
     for (int i = 1; i <= m; i++) {
-        int edgeIndex, u, v;
-        double w;
-
-        fscanf(fp, "%d %d %d %lf", &edgeIndex, &u, &v, &w);
+    int edgeIndex, u, v;
+    double w;
+    fscanf(fp, "%d %d %d %lf", &edgeIndex, &u, &v, &w);
 
     if (u < 1 || u > n || v < 1 || v > n) {
         fprintf(stderr, "Error: Edge with invalid vertices\n");
         // Handle error or exit
     }
 
-         insertIntoAdjacencyList(ADJ, u, v, w, edgeIndex, flag);
-    
-    // Assuming the argv[2] holds the graph type
+    insertIntoAdjacencyList(ADJ, u, v, w, edgeIndex, flag); // insert at front
     if (strcmp(argv[2], "UndirectedGraph") == 0) {
-        insertIntoAdjacencyList(ADJ, v, u, w, edgeIndex, flag);
+        insertIntoAdjacencyList(ADJ, v, u, w, edgeIndex, flag); // also insert reverse edge for undirected graph
     }
 }
 
     fclose(fp);
-    
-    while(1) {
+    int key;
+    int returnV;
+    char Word[100];
+    while (1) {
 
         returnV = nextInstruction(Word, &key);
 
@@ -101,23 +98,38 @@ if (V == NULL) {
             continue;
         }
 
-        if(strcmp(Word, "Stop")==0){
-            exit(0);
-        }
-
+        if (strcmp(Word, "Stop") == 0) {
+            break;
+            }
+        
         if (strcmp(Word, "PrintADJ") == 0) {
             PrintADJ(ADJ, n);
             continue;
-        }
+            }
 
-        if(strcmp(Word, "SinglePair") == 0){
+        if (strcmp(Word, "SinglePair") == 0) {
             int src, dest;
-            fscanf(stdin, "%d %d", &src, &dest); // Read source and destination
-            dijkstra(V, ADJ, n, src, dest); // Apply Dijkstra's algorithm
-            lastSrc = src;
-            lastDest = dest;
+            fscanf(stdin, "%d %d", &src, &dest);
+            STACK *pathStack = new_stack();
+            dijkstra(V, ADJ, n, src, dest, pathStack);
+            
+            if (V[dest - 1]->d == DBL_MAX) {
+    printf("%f", V[dest - 1]->d);
+                printf("No path from %d to %d.\n", src, dest);
+            } else {
+                printf("Shortest path weight from %d to %d is: %f\n", src, dest, V[dest - 1]->d);
+            }
             continue;
-        }
+            }
+
+        if (strcmp(Word, "PrintPath") == 0) {
+            int src, dest;
+            printf("Enter source and destination for path: ");
+            fscanf(stdin,"%d %d", &src, &dest);
+            printPath(V, n, src, dest);
+            continue;
+            }
+
 
         if(strcmp(Word, "SingleSource")==0){
             continue;
@@ -126,24 +138,27 @@ if (V == NULL) {
         if(strcmp(Word, "PrintLength")==0){
             continue;
         }
-
-        if(strcmp(Word, "PrintPath") == 0){
-            int s, t;
-            fscanf(stdin, "%d %d", &s, &t); // Read s and t;
-            if (s == lastSrc && t == lastDest) {
-                printPath(V, s, t); // Print the path if valid
-    }    else {
-            printf("Invalid request\n");
-    }
-            continue;
+    
         }
 
+            
+        // Add more commands as needed
+    
 
 
 
+
+    // Free dynamically allocated memory
+    for (int i = 1; i <= n; i++) {
+        free(V[i]);
+        while (ADJ[i]) {
+            NODE *temp = ADJ[i];
+            ADJ[i] = ADJ[i]->next;
+            free(temp);
+        }
     }
-    
-    
-    return 0;    
-    // TODO: Don't forget to free the dynamically allocated memory before program exit
+    free(V);
+    free(ADJ);
+
+    return 0;
 }
