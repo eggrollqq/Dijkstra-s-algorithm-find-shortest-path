5c185af82c69062d126470e092126fe1f75196bf
Evan <103800106+eggrollqq@users.noreply.github.com>
2023-11-09 09:13:53 -0700

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -4,22 +4,23 @@
 #include <float.h>
 
 
-void swap(pELEMENT *a, pELEMENT *b) {
-    pELEMENT temp = *a;
+
+void swap(VERTEX **a, VERTEX **b) {
+    VERTEX *temp = *a;
     *a = *b;
     *b = temp;
 }
 
 bool IsMinHeap(HEAP *heap) {
-    if (heap == NULL || heap->H == NULL || heap->size == 0) {
+    if (!heap || !heap->H || heap->size == 0) {
         return false;
     }
- 
-    for (int i = heap->size/2; i >= 1 ; i--) {
-        if (heap->H[i]->d > heap->H[2*i]->d){
+
+    for (int i = heap->size / 2; i >= 1; --i) {
+        if (heap->H[i]->d > heap->H[2 * i]->d) {
             return false;
         }
-        if (2*i + 1 <= heap->size && heap->H[i]->d > heap->H[2*i + 1]->d) {
+        if (2 * i + 1 <= heap->size && heap->H[i]->d > heap->H[2 * i + 1]->d) {
             return false;
         }
     }
@@ -27,18 +28,15 @@ bool IsMinHeap(HEAP *heap) {
     return true;
 }
 
+
 void Heapify(HEAP *h, int i) {
-    
-    int smallest;
-    int l = 2 * i ;
+    int smallest = i;
+    int l = 2 * i;
     int r = 2 * i + 1;
 
     if (l <= h->size && h->H[l]->d < h->H[i]->d) {
         smallest = l;
-    } else {
-        smallest = i;
     }
-
     if (r <= h->size && h->H[r]->d < h->H[smallest]->d) {
         smallest = r;
     }
@@ -46,79 +44,68 @@ void Heapify(HEAP *h, int i) {
     if (smallest != i) {
         swap(&h->H[i], &h->H[smallest]);
         Heapify(h, smallest);
-        
     }
 }
 
-void BuildHeap(HEAP *h, ELEMENT **A, int n) {
-    
+
+void BuildHeap(HEAP *h, VERTEX **A, int n) {
     h->size = n;
     h->H = A;
 
-    for (int i = h->size / 2 ; i >= 1; i--) { 
+    for (int i = h->size / 2; i >= 1; --i) {
         Heapify(h, i);
     }
-    
 }
 
-ELEMENT *ExtractMin(HEAP *h) {
-
-    if (h == NULL) {
-        fprintf(stderr, "Error: heap is NULL");
+VERTEX *ExtractMin(HEAP *h) {
+    if (!h) {
+        fprintf(stderr, "Error: heap is NULL\n");
         return NULL;
     }
-
     if (h->size < 1) {
-        fprintf(stderr, "Error: heap is empty");
+        fprintf(stderr, "Error: heap is empty\n");
         return NULL;
     }
-    else{
-    ELEMENT *min = h->H[1];
-    h->H[1] = h->H[h->size];
-    h->size--;
+
+    VERTEX *min = h->H[1];
+    h->H[1] = h->H[h->size--];
     Heapify(h, 1);
 
-
     return min;
-    }
 }
 
 void DecreaseKey(HEAP *h, int i, double newKey) {
+    if (!h || i < 1 || i > h->size) {
+        fprintf(stderr, "Error: Invalid index for heap decrease key\n");
+        return;
+    }
 
     h->H[i]->d = newKey;
-    while (i > 1 && h->H[i / 2] != NULL && h->H[i / 2]->d > h->H[i]->d) {
+    while (i > 1 && h->H[i / 2]->d > h->H[i]->d) {
         swap(&h->H[i], &h->H[i / 2]);
-        i = i / 2;
+        i /= 2;
     }
 }
 
 
-void Insertion(HEAP *h, ELEMENT *x) {
+void Insertion(HEAP *h, VERTEX *x) {
     if (h->size == h->capacity) {
         fprintf(stderr, "Error: heap is full\n");
         return;
     }
 
-    h->size++;
+    h->H[++h->size] = x;
     int i = h->size;
-    h->H[i] = x;
-
     while (i > 1 && h->H[i / 2]->d > h->H[i]->d) {
         swap(&h->H[i], &h->H[i / 2]);
-        i = i / 2;
-    
+        i /= 2;
+    }
 }
 
-}
 
-
-void freeHeap(HEAP* h) {
-    if(h != NULL) {
-        for(int i = 1; i <= h->size; i++) {
-            free(h->H[i]);
-        }
+void freeHeap(HEAP *h) {
+    if (h) {
         free(h->H);
         free(h);
     }
 }
-
