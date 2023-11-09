854a02c722acf07a079703e72eaa9e20547580b4
Evan <103800106+eggrollqq@users.noreply.github.com>
2023-11-09 09:14:59 -0700

    Update stack.h

---
+++ stack.h
@@ -0,0 +1,14 @@
+
+#ifndef _stack_H
+#define _stack_H 1
+#include "data_structures.h"
+
+STACK *new_stack();
+int stack_empty(STACK *stack);
+void stack_push(STACK *stack, VERTEX *vertex);
+VERTEX *stack_top(STACK *stack);
+VERTEX *stack_pop(STACK *stack);
+void free_stack(STACK *stack);
+
+
+#endif
