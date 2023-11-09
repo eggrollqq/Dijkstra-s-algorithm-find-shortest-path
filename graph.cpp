7242bd23245ebc48b7d42f3d7af79b9b0c989a9f
Evan <103800106+eggrollqq@users.noreply.github.com>
2023-11-09 09:13:26 -0700

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,16 +1,24 @@
 #include <cmath>
 #include <stdio.h>
 #include <stdlib.h>
+#include "data_structures.h"
 #include "graph.h"
 #include "heap.h"
+#include "stack.h"
+#include <cfloat>
 
-void insertIntoAdjacencyList(pNODE* ADJ, int u, int v, double w, int edgeIndex, int flag) {
-    pNODE new_node = (pNODE) malloc(sizeof(NODE));
+
+void insertIntoAdjacencyList(NODE **ADJ, int u, int v, double w, int edgeIndex, int flag) {
+    NODE *new_node = (NODE *)malloc(sizeof(NODE));
+    if (!new_node) {
+        fprintf(stderr, "Error: Memory allocation failed for new node\n");
+        return;
+    }
+
     new_node->index = edgeIndex;
     new_node->u = u;
     new_node->v = v;
     new_node->w = w;
-    new_node->next = NULL;
 
     if (flag == 1) {  // insert at the front
         new_node->next = ADJ[u - 1];
@@ -19,7 +27,7 @@ void insertIntoAdjacencyList(pNODE* ADJ, int u, int v, double w, int edgeIndex, 
         if (ADJ[u - 1] == NULL) {
             ADJ[u - 1] = new_node;
         } else {
-            pNODE temp = ADJ[u - 1];
+            NODE *temp = ADJ[u - 1];
             while (temp->next != NULL) {
                 temp = temp->next;
             }
@@ -28,67 +36,78 @@ void insertIntoAdjacencyList(pNODE* ADJ, int u, int v, double w, int edgeIndex, 
     }
 }
 
-void PrintADJ(NODE* ADJ[], int n) {
-    for (int i = 1; i <= n; i++) {
-        fprintf(stdout, "ADJ[%d]:", i);
-        NODE* current = ADJ[i - 1]; // Change this line
-        while (current != NULL) {
-            fprintf(stdout, "-->[%d %d: %.2f]", current->u, current->v, current->w);
+void PrintADJ(NODE **ADJ, int n) {
+    for (int i = 1; i <= n; ++i) {
+        fprintf(stdout, "ADJ[%d]:", i); // Use i + 1 for 1-based indexing in output
+        NODE *current = ADJ[i - 1];
+        while (current) {
+            fprintf(stdout,"-->[%d %d: %.2f]", current->u, current->v, current->w);
             current = current->next;
         }
         fprintf(stdout, "\n");
     }
 }
 
-void printPath(pVERTEX *V, int source, int destination) {
-    if (V[destination]->d == INFINITY) {
-        printf("There is no path from %d to %d.\n", source, destination);
+void printPath(VERTEX **V, int n, int source, int destination) {
+    if (V[destination - 1]->d == INFINITY) {
+        fprintf(stdout, "There is no path from %d to %d.\n", source, destination);
         return;
     }
 
-    // Your stack implementation seems to be incorrect. Here's a correction:
-    int stackSize = 0;
+    STACK *pathStack = new_stack();
     int current = destination;
-    while (current != source) { // The loop should continue until it reaches the source
-        stackSize++;
-        current = V[current]->pi; // Make sure V[current]->pi is set correctly in dijkstra
+    while (current != source) {
+        stack_push(pathStack, V[current - 1]);
+        current = V[current - 1]->pi;
     }
-    stackSize++; // Include the source in the path size
+    stack_push(pathStack, V[source - 1]);
 
-    int *pathStack = (int *)malloc(stackSize * sizeof(int));
-    double *distStack = (double *)malloc(stackSize * sizeof(double));
-    current = destination;
-    int i = 0;
-    while (current != 0) {
-        pathStack[i] = current;
-        distStack[i] = V[current]->d;
-        current = V[current]->pi; // This should lead back to the source
-        i++;
-    }
-
-    printf("The shortest path from %d to %d is:\n", source, destination);
-    for (int j = stackSize - 1; j >= 0; j--) { // Reverse the stack to print in correct order
-        printf("[%d: %5.2f]", pathStack[j], distStack[j]);
-        if (j > 0) {
-            printf("-->");
+    fprintf(stdout, "The shortest path from %d to %d is:\n", source, destination);
+    while (!stack_empty(pathStack)) {
+        VERTEX *vertex = stack_top(pathStack);
+        fprintf(stdout, "[%d: %.2f]", vertex->index, vertex->d);
+        stack_pop(pathStack);
+        if (!stack_empty(pathStack)) {
+            fprintf(stdout, "-->");
         }
     }
-    printf(".\n");
+    printf("\n");
+    free_stack(pathStack);
+}
 
-    free(pathStack);
-    free(distStack);
+HEAP *InitializeHeap(int capacity) {
+    HEAP *heap = (HEAP *)malloc(sizeof(HEAP));
+    if (!heap) {
+        fprintf(stderr, "Error: Memory allocation failed for heap\n");
+        return NULL;
+    }
+
+    heap->H = (VERTEX **)malloc((capacity + 1) * sizeof(VERTEX *));
+    if (!heap->H) {
+        fprintf(stderr, "Error: Memory allocation failed for heap array\n");
+        free(heap);
+        return NULL;
+    }
+
+    heap->capacity = capacity;
+    heap->size = 0;
+    for (int i = 1; i <= capacity; i++) {
+        heap->H[i] = NULL;
+    }
+
+    return heap;
 }
 
 
-void dijkstra(pVERTEX *V, pNODE *ADJ, int n, int source, int target) {
+void dijkstra(VERTEX **V, NODE **ADJ, int n, int source, int target, STACK *pathStack) {
     // Initialize all vertices
     for (int i = 1; i <= n; i++) {
-        V[i]->d = INFINITY;
+        V[i]->d = 0;
         V[i]->pi = 0;
+        V[i]->index = i;
     }
     V[source]->d = 0;
 
-    // Create a min-heap and insert all vertices
     HEAP *heap = InitializeHeap(n);
     for (int i = 1; i <= n; i++) {
         Insertion(heap, V[i]);
@@ -97,50 +116,38 @@ void dijkstra(pVERTEX *V, pNODE *ADJ, int n, int source, int target) {
     while (heap->size > 0) {
         ELEMENT *u = ExtractMin(heap);
 
-        // Stop if the target is reached
+if (u->index < 1 || u->index > n) {
+            fprintf(stderr, "Error: Invalid index %d extracted from heap\n", u->index);
+            continue;
+        }
+
+        stack_push(pathStack, V[u->index]); // Push vertex onto the stack
+
         if (u->index == target) {
             break;
         }
 
-        // Relax edges
-        pNODE temp = ADJ[u->index];
+        
+double count = 0;
+for (int i = 1; i <= n; ++i) {
+        NODE *temp = ADJ[u->index -1];
+        u->index++;
         while (temp != NULL) {
-            int v = temp->v;
-            if (V[v]->d > V[u->index]->d + temp->w) {
-                V[v]->d = V[u->index]->d + temp->w;
-                V[v]->pi = u->index;
-                DecreaseKey(heap, V[v]->position, V[v]->d);
+            int v = temp->v;      
+printf("for edge %d->%d: weight: %.2lf\n", temp->u, temp->v, temp->w);
+printf("V[v]->d:%lf >V[u->index]->d: %.2lf + %.2lf\n" , ADJ[v]->w,ADJ[u->index-1]->w, temp->w);               
+printf("%.2lf\n", count = count + temp->w);      
+printf("%d", u->index);
+printf("      %.2lf\n", ADJ[u->index-1]->w);
+            if (ADJ[v]->w > ADJ[u->index]->w + temp->w) {
+                ADJ[v]->w = ADJ[u->index]->w + temp->w; // Update the distance
+                ADJ[v]->pi = u->index; // Update the predecessor
+                DecreaseKey(heap, V[v]->position, V[v]->d); // Update the position in the heap
             }
             temp = temp->next;
         }
     }
-    free(heap);
+
+    freeHeap(heap);
 }
-
-HEAP *InitializeHeap(int capacity) {
-    HEAP *heap = (HEAP *)malloc(sizeof(HEAP));
-    if (heap == NULL) {
-        fprintf(stderr, "Error: Memory allocation failed for heap\n");
-        free(heap);
-        return NULL;
-    }
-
-    // The heap array (H) will store pointers to VERTEX, thus the type is pELEMENT*
-    heap->H = (pELEMENT *)malloc((capacity + 1) * sizeof(pELEMENT));
-    if (heap->H == NULL) {
-        fprintf(stderr, "Error: Memory allocation failed for heap array\n");
-        free(heap);
-        return NULL;
-    }
-
-    heap->capacity = capacity;
-    heap->size = 0;
-
-    // Initialize all elements to NULL
-    for (int i = 1; i <= capacity; i++) {
-        heap->H[i] = NULL;
-    }
-
-    return heap;
-
 }
