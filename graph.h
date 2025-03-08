2df8b3abde95010a5f92041aa4276b52ea585aa3
Evan <103800106+eggrollqq@users.noreply.github.com>
2025-03-07 22:37:49 -0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,4 +1,3 @@
-
 #ifndef _graph_H
 #define _graph_H 1
 #include "data_structures.h"
@@ -7,6 +6,9 @@
 void insertIntoAdjacencyList(NODE **ADJ, int u, int v, double w, int edgeIndex, int flag);
 void PrintADJ(NODE **ADJ, int n);
 void printPath(VERTEX **V, int n, int source, int destination);
-void dijkstra(VERTEX **V, NODE **ADJ, int n, int source, int target, STACK *pathStack);
+void dijkstra(VERTEX **V, NODE **ADJ, int n, int source, int target);
+void SingleSource(VERTEX **V, NODE **ADJ, int n, int source, int network03Flag, int SingleSourceCountFlag);
+void PrintLength(VERTEX **V, int n, int source, int destination);
+int findMinDist(VERTEX **V, bool *sptSet, int n);
 
 #endif
